c3509a35ec2f86b594e47e7c6eb80f7fc23382b9
2375-days <15293563204@163.com>
2021-12-08 11:28:39 +0800

    feat(models,services):添加Tourist模型和仓储
    添加TourictRoute模型类设计；
    添加TouristRoutePictures模型类设计；
    添加TouristRoute仓储接口；
    添加TouristRoute的Mock仓储；

---
+++ Tourist/models/TouristRoutePicture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tourist.models
+{
+    public class TouristRoutePicture
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public Guid TouristRouteId { get; set; }
+        public TouristRoute TouristRoute { get; set; }
+    }
+}
