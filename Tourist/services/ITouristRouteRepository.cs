5c21e6930cc577c46ef3bc726f633794941d7253
2375-days <15293563204@163.com>
2021-12-08 11:12:37 +0800

    feat(models,services):添加Tourist模型和仓储
    添加TourictRoute模型类设计；
    添加TouristRoutePictures模型类设计；
    添加TouristRoute仓储接口；
    添加TouristRoute的Mock仓储；

---
+++ Tourist/services/ITouristRouteRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Tourist.models;
+
+namespace Tourist.services
+{
+    public interface ITouristRouteRepository
+    {
+        IEnumerable<TouristRoute> GetTouristRoutes();
+        TouristRoute GetTouristRoute(Guid id);
+    }
+}
---
+++ Tourist/services/MockTouristRouteRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tourist.models;
+
+namespace Tourist.services
+{
+    public class MockTouristRouteRepository : ITouristRouteRepository
+    {
+        private List<TouristRoute> _routes;
+        public MockTouristRouteRepository()
+        {
+            if (_routes == null)
+            {
+                InitializeTouristRoute();
+            }
+        }
+
+        public List<TouristRoute> InitializeTouristRoute()
+        {
+            _routes = new List<TouristRoute>
+            {
+                new TouristRoute{
+                    Id = Guid.NewGuid(),
+                    Title = "黄山",
+                    Description = "黄山真好玩",
+                    OriginalPrice = 1299,
+                    Features = "<p>吃喝玩乐，行住游娱</p>",
+                    Notes="<p>小心危险</p>"
+                },
+                new TouristRoute {
+                    Id = Guid.NewGuid(),
+                    Title = "华山",
+                    Description="华山真好玩",
+                    OriginalPrice = 1299,
+                    Features = "<p>吃住行游购娱</p>",
+                    Fees = "<p>交通费用自理</p>",
+                    Notes="<p>小心危险</p>"
+                }
+            };
+            return _routes;
+        }
+        public TouristRoute GetTouristRoute(Guid id)
+        {
+            return _routes.FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<TouristRoute> GetTouristRoutes()
+        {
+            return _routes;
+        }
+    }
+}
