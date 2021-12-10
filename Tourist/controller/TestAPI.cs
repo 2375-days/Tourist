e7823752e6bad80000a1211a317f4797d17c0e92
2375-days <15293563204@163.com>
2021-12-10 11:18:32 +0800

    fix(database);修复数据中文乱码问题
    更改种子数据Json文件编码为UTF8；
    重新迁移数据库

--- Tourist/controller/TestAPI.cs
+++ Tourist/controller/TestAPI.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Tourist.API.models;
+using System.Text;
+using System;
 
 namespace Tourist.API.controller
 {
@@ -9,6 +15,19 @@ namespace Tourist.API.controller
         public IEnumerable<string> Test()
         {
             return new string[] { "val1", "val2" };
+        }
+
+        [Route("api/testFileRead")]
+        public IActionResult TestFileRead()
+        {
+            var touristRouteJsonData = System.IO.File.ReadAllText(
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + 
+                @"/Database/touristRoutesMockData.json"
+                );
+            IList<TouristRoute> touristRoutes = JsonConvert.DeserializeObject<IList<TouristRoute>>(touristRouteJsonData);
+            Console.WriteLine(touristRouteJsonData);
+            Console.WriteLine(touristRoutes);
+            return Ok(touristRouteJsonData);
         }
     }
 }
