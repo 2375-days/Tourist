8b3c711154cfbcfcd0d37d1f7849d1fd03e78ee5
2375-days <15293563204@163.com>
2021-12-13 16:47:43 +0800

    Revert "Revert "feat(services, controller):给GetTouristRoutes接口增加按关键字匹配功能""
    This reverts commit 4ba7beb2a2544b4e61e2b8c5598dcdb3d865cb3b.

--- Tourist/services/MockTouristRouteRepository.cs
+++ Tourist/services/MockTouristRouteRepository.cs
@@ -5,63 +5,68 @@ using Tourist.API.Models;
 
 namespace Tourist.API.Services
 {
-    public class MockTouristRouteRepository : ITouristRouteRepository
-    {
-        private List<TouristRoute> _routes;
-        public MockTouristRouteRepository()
-        {
-            if (_routes == null)
-            {
-                InitializeTouristRoute();
-            }
-        }
+    //public class MockTouristRouteRepository : ITouristRouteRepository
+    //{
+    //    private List<TouristRoute> _routes;
+    //    public MockTouristRouteRepository()
+    //    {
+    //        if (_routes == null)
+    //        {
+    //            InitializeTouristRoute();
+    //        }
+    //    }
 
-        public void InitializeTouristRoute()
-        {
-            _routes = new List<TouristRoute>
-            {
-                new TouristRoute{
-                    Id = Guid.NewGuid(),
-                    Title = "黄山",
-                    Description = "黄山真好玩",
-                    OriginalPrice = 1299,
-                    Features = "<p>吃喝玩乐，行住游娱</p>",
-                    Notes="<p>小心危险</p>"
-                },
-                new TouristRoute {
-                    Id = Guid.NewGuid(),
-                    Title = "华山",
-                    Description="华山真好玩",
-                    OriginalPrice = 1299,
-                    Features = "<p>吃住行游购娱</p>",
-                    Fees = "<p>交通费用自理</p>",
-                    Notes="<p>小心危险</p>"
-                }
-            };
-        }
-        public TouristRoute GetTouristRoute(Guid id)
-        {
-            return _routes.FirstOrDefault(x => x.Id == id);
-        }
+    //    public void InitializeTouristRoute()
+    //    {
+    //        _routes = new List<TouristRoute>
+    //        {
+    //            new TouristRoute{
+    //                Id = Guid.NewGuid(),
+    //                Title = "黄山",
+    //                Description = "黄山真好玩",
+    //                OriginalPrice = 1299,
+    //                Features = "<p>吃喝玩乐，行住游娱</p>",
+    //                Notes="<p>小心危险</p>"
+    //            },
+    //            new TouristRoute {
+    //                Id = Guid.NewGuid(),
+    //                Title = "华山",
+    //                Description="华山真好玩",
+    //                OriginalPrice = 1299,
+    //                Features = "<p>吃住行游购娱</p>",
+    //                Fees = "<p>交通费用自理</p>",
+    //                Notes="<p>小心危险</p>"
+    //            }
+    //        };
+    //    }
+    //    public TouristRoute GetTouristRoute(Guid id)
+    //    {
+    //        return _routes.FirstOrDefault(x => x.Id == id);
+    //    }
 
-        public IEnumerable<TouristRoute> GetTouristRoutes()
-        {
-            return _routes;
-        }
+    //    public IEnumerable<TouristRoute> GetTouristRoutes()
+    //    {
+    //        return _routes;
+    //    }
 
-        public IEnumerable<TouristRoutePicture> GetPicturesByTouristRouteId(Guid touristRouteId)
-        {
-            throw new NotImplementedException();
-        }
+    //    public IEnumerable<TouristRoutePicture> GetPicturesByTouristRouteId(Guid touristRouteId)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
 
-        public bool TouristRouteExists(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+    //    public bool TouristRouteExists(Guid id)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
 
-        public TouristRoutePicture GetPicture(int pictureId)
-        {
-            throw new NotImplementedException();
-        }
-    }
+    //    public TouristRoutePicture GetPicture(int pictureId)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public IEnumerable<TouristRoute> GetTouristRoutes(string keyword)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
 }
